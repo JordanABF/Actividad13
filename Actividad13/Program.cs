be8b9b31ea4ae5b5358cfa6aa64da0173d4442f7
CDVCvask <cesarcamposeco415@gmail.com>
2026-03-12 20:06:33 -0600

    Agregar archivos de proyecto.

---
+++ Actividad13/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Dispositivos
+{
+    public string Nombre;
+    public double rendimiento1;
+    public double rendimiento2;
+    public double rendimiento3;
+
+    public double promediorendimiento ()
+    { return (rendimiento1+ rendimiento2 + rendimiento3)/3;  }
+
+
+}
